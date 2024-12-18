c1e36ec4bcc7d278fd9c25c3616d9d15492a1541
dong <dho0647@naver.com>
2024-12-19 06:35:39 +0900

    실전2 - 회원 조회 API

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -41,4 +41,11 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        // update 시 merge 사용 X
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
